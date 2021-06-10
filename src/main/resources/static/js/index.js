7911468b8901a66257b42e1779a52dd3a17971f8
ckdgus08 <ckdgus0808@naver.com>
2021-06-10 17:59:03 +0900

    refactor: 변수명, 메서드명 한글로 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,79 +1,59 @@
-push = [false, false, false];
-let array = [
+버튼_현재상태 = [false, false, false];
+let 버튼_눌린상태_배열 = [
     [false, false, false], [true, false, false],
     [false, true, false], [false, false, true],
     [true, true, false], [true, false, true],
     [false, true, true], [true, true, true]];
-value_cache = [];
+수신부_결과값_배열 = [];
 
 window.onload = function () {
 
-    let inch_slide = new Slider('#random', {});
+    new Slider('#random', {});
 
-    change(push);
-    check();
-
+    각_지점_계산하고_결과반영(버튼_현재상태);
+    목표하는_dx값이_구간의_최소dx보다_큰지_체크하기();
 
     document.getElementById("dx").onchange = function () {
-        check();
+        목표하는_dx값이_구간의_최소dx보다_큰지_체크하기();
     }
-
     document.getElementById("button_0").onclick = function () {
-
-        change_button_status(0)
+        버튼_클릭(0)
     }
 
     document.getElementById("button_1").onclick = function () {
-
-        change_button_status(1)
+        버튼_클릭(1)
     }
 
     document.getElementById("button_2").onclick = function () {
-
-        change_button_status(2)
+        버튼_클릭(2)
     }
-    let data = getData();
+    let 데이터배열 = 기록된_데이터_가져오기();
 
     document.getElementById("target_value").onclick = function () {
-        for (const value of data) {
+        for (const 데이터 of 데이터배열) {
             setTimeout(function exec() {
-                go_change(value)
+                수신부_데이터로_어느버튼_눌렸는지_역추적(데이터)
             }, 0);
         }
         document.getElementById("target_value_result").innerText = "완료"
-        // document.getElementById("target_value_result").classList.add("no")
-        // document.getElementById("target_value_result").classList.remove("ok")
-        // document.getElementById("target_value_result").innerText = "적절하지 않은 수"
     }
+
+    alert(수신부_결과값_배열)
 }
 
-function change_button_status(index) {
-    if (push[index]) {
-        document.getElementById("button_" + index).style.height = "50px";
-        document.getElementById("button_" + index).classList.remove("push")
-        document.getElementById("button_" + index).classList.add("unpush")
-    } else {
-        document.getElementById("button_" + index).style.height = "150px";
-        document.getElementById("button_" + index).classList.add("push")
-        document.getElementById("button_" + index).classList.remove("unpush")
-    }
-    push[index] = !push[index]
-    change(push)
-}
+function 수신부_데이터로_어느버튼_눌렸는지_역추적(수신부데이터) {
 
-function go_change(value) {
-    let dx = document.getElementById("dx").value;
-    document.getElementById("target_value_result").innerText = value;
+    document.getElementById("target_value_result").innerText = 수신부데이터;
 
-    for (let i = 1; i < value_cache.length; i++) {
-        if (value_cache[i] - (value_cache[i] * 0.05) < value && value_cache[i] + (value_cache[i] * 0.05) >= value) {
-            push[0] = !array[i][0];
-            push[1] = !array[i][1];
-            push[2] = !array[i][2];
+    for (let i = 1; i < 수신부_결과값_배열.length; i++) {
+        if (수신부_결과값_배열[i] - (수신부_결과값_배열[i] * 0.04) < 수신부데이터 && 수신부_결과값_배열[i] + (수신부_결과값_배열[i] * 0.04) >= 수신부데이터) {
+            버튼_현재상태[0] = !버튼_눌린상태_배열[i][0];
+            버튼_현재상태[1] = !버튼_눌린상태_배열[i][1];
+            버튼_현재상태[2] = !버튼_눌린상태_배열[i][2];
 
-            change_button_status(0)
-            change_button_status(1)
-            change_button_status(2)
+            버튼_클릭(0)
+            버튼_클릭(1)
+            버튼_클릭(2)
 
             document.getElementById("target_value_result").classList.add("ok")
             document.getElementById("target_value_result").classList.remove("no")
@@ -82,98 +62,102 @@ function go_change(value) {
     }
 }
 
-function check() {
-    let dx = document.getElementById("dx").value;
-
-    let value = [];
-
-    for (let i = 0; i < array.length; i++) {
-        value[i] = change_for_check(array[i]);
-        value_cache[i] = value[i];
+function 버튼_클릭(index) {
+    if (버튼_현재상태[index]) {
+        document.getElementById("button_" + index).style.height = "50px";
+        document.getElementById("button_" + index).classList.remove("push")
+        document.getElementById("button_" + index).classList.add("unpush")
+    } else {
+        document.getElementById("button_" + index).style.height = "150px";
+        document.getElementById("button_" + index).classList.add("push")
+        document.getElementById("button_" + index).classList.remove("unpush")
     }
+    버튼_현재상태[index] = !버튼_현재상태[index]
+    각_지점_계산하고_결과반영(버튼_현재상태)
+}
 
-    let min = 10000000;
-    for (let i = 0; i < value.length; i++) {
-        for (let j = 0; j < value.length; j++) {
-            let temp = Math.abs(value[i] - value[j]);
-            if (temp !== 0 && min > temp)
-                min = temp;
+// 목표하는 dx값이 구간의 최소 dx보다 큰지 체크
+function 목표하는_dx값이_구간의_최소dx보다_큰지_체크하기() {
+    let 목표_dx값 = parseInt(document.getElementById("dx").value)
+
+    for (let i = 0; i < 버튼_눌린상태_배열.length; i++)
+        수신부_결과값_배열[i] = 수신부값계산하기(버튼_눌린상태_배열[i]);
+
+    let 최소값 = 10000000;
+    for (let i = 0; i < 수신부_결과값_배열.length; i++) {
+        for (let j = 0; j < 수신부_결과값_배열.length; j++) {
+            let 구간차이값 = Math.abs(수신부_결과값_배열[i] - 수신부_결과값_배열[j]);
+            if (구간차이값 !== 0 && 최소값 > 구간차이값)
+                최소값 = 구간차이값;
         }
     }
 
-    let dx_result = document.getElementsByClassName("dx_result")[0];
-    if (parseInt(dx) <= min) {
-        dx_result.innerHTML = "최소 dx = " + min;
-        dx_result.classList.add("ok");
-        dx_result.classList.remove("no");
+    let 구간최소값 = document.getElementsByClassName("dx_result")[0];
+    if (목표_dx값 <= 최소값) {
+        구간최소값.innerHTML = "최소 dx = " + 최소값;
+        구간최소값.classList.add("ok");
+        구간최소값.classList.remove("no");
     } else {
-        dx_result.innerHTML = "최소 dx = " + min;
-        dx_result.classList.add("no");
-        dx_result.classList.remove("ok");
+        구간최소값.innerHTML = "최소 dx = " + 최소값;
+        구간최소값.classList.add("no");
+        구간최소값.classList.remove("ok");
     }
-
 }
 
-function change_for_check(isPush) {
+// 수신부 값 계산하기
+function 수신부값계산하기(isPush) {
 
-    let initValue = document.getElementsByClassName("randomTag")[1].innerHTML;
+    let 초기값 = parseInt(document.getElementsByClassName("randomTag")[1].innerHTML)
+    let 투과율1번지점 = parseInt(document.getElementById("number_1").value)
+    let 투과율2번지점 = parseInt(document.getElementById("number_2").value)
+    let 투과율3번지점 = parseInt(document.getElementById("number_3").value)
 
-    let number1 = document.getElementById("number_1").value;
-    let number2 = document.getElementById("number_2").value;
-    let number3 = document.getElementById("number_3").value;
-
-    let value1 = document.getElementById("value_1");
-    let value2 = document.getElementById("value_2");
-    let value3 = document.getElementById("value_3");
-
-    initValue = parseInt(initValue);
+    let 수신부값 = 초기값;
 
     if (isPush[0])
-        initValue = parseInt(initValue) * (parseInt(number1) / 100);
+        수신부값 = 수신부값 * 투과율1번지점 / 100;
 
     if (isPush[1])
-        initValue = initValue * (parseInt(number2) / 100);
+        수신부값 = 수신부값 * 투과율2번지점 / 100;
 
     if (isPush[2])
-        initValue = initValue * (parseInt(number3) / 100);
-
-    return parseInt(initValue);
+        수신부값 = 수신부값 * 투과율3번지점 / 100;
+    return 수신부값;
 }
 
+// 각 지점 값 계산하고_결과반영
+function 각_지점_계산하고_결과반영(isPush) {
 
-function change(isPush) {
+    let 초기값 = parseInt(document.getElementsByClassName("randomTag")[1].innerHTML)
+    let 투과율1번지점 = parseInt(document.getElementById("number_1").value)
+    let 투과율2번지점 = parseInt(document.getElementById("number_2").value)
+    let 투과율3번지점 = parseInt(document.getElementById("number_3").value)
 
-    let initValue = document.getElementsByClassName("randomTag")[1].innerHTML;
+    let _1번지점텍스트 = document.getElementById("value_1");
+    let _2번지점텍스트 = document.getElementById("value_2");
+    let _3번지점텍스트 = document.getElementById("value_3");
 
-    let number1 = document.getElementById("number_1").value;
-    let number2 = document.getElementById("number_2").value;
-    let number3 = document.getElementById("number_3").value;
-
-    let value1 = document.getElementById("value_1");
-    let value2 = document.getElementById("value_2");
-    let value3 = document.getElementById("value_3");
-
-    initValue = parseInt(initValue);
+    let 수신부값 = 초기값;
 
     if (isPush[0])
-        initValue = parseInt(initValue) * (parseInt(number1) / 100);
-    value1.innerText = parseInt(initValue).toString();
+        수신부값 = 수신부값 * 투과율1번지점 / 100;
+    _1번지점텍스트.innerText = 수신부값.toString();
+
 
     if (isPush[1])
-        initValue = initValue * (parseInt(number2) / 100);
-    value2.innerText = parseInt(initValue).toString();
+        수신부값 = 수신부값 * 투과율2번지점 / 100;
+    _2번지점텍스트.innerText = 수신부값.toString();
 
     if (isPush[2])
-        initValue = initValue * (parseInt(number3) / 100);
+        수신부값 = 수신부값 * 투과율3번지점 / 100;
+    _3번지점텍스트.innerText = 수신부값.toString();
 
-    value3.innerText = parseInt(initValue).toString();
-
-    return parseInt(initValue);
+    return 수신부값;
 }
 
-function getData() {
+function 기록된_데이터_가져오기() {
 
-    result = [];
+    let result = [];
     $.ajax({
         async: false,
         method: 'GET',
