9c40a5d339ef57a185c9f415bc80bd2bf2e6db95
ckdgus08 <ckdgus0808@naver.com>
2021-07-11 22:48:24 +0900

    refactor: influxDB 의존성 삭제, SPI 데이터 실시간 연동 작업 ing~

--- src/main/java/com/github/ckdgus08/entity/OpticalData.java
+++ src/main/java/com/github/ckdgus08/entity/OpticalData.java
@@ -1,42 +1,42 @@
-package com.github.ckdgus08.entity;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.influxdb.annotation.Column;
-import org.influxdb.annotation.Measurement;
-import org.influxdb.annotation.TimeColumn;
-
-import java.time.Instant;
-
-@Measurement(name = "optical_data")
-@Getter
-@NoArgsConstructor
-@ToString(of = {"time", "uuid", "optical_value"})
-public class OpticalData {
-
-    @TimeColumn
-    @Column(name = "time")
-    private Instant time;
-
-    @Column(name = "uuid")
-    private String uuid;
-
-    @Column(name = "optical_value")
-    private int optical_value;
-
-
-    public OpticalData(String uuid, int optical_value) {
-        this.optical_value = optical_value;
-        this.uuid = uuid;
-
-        this.time = Instant.now();
-    }
-
-    public OpticalData(Instant time, String uuid, int optical_value) {
-        this.time = time;
-        this.optical_value = optical_value;
-        this.uuid = uuid;
-    }
-
-}
+//package com.github.ckdgus08.entity;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//import org.influxdb.annotation.Column;
+//import org.influxdb.annotation.Measurement;
+//import org.influxdb.annotation.TimeColumn;
+//
+//import java.time.Instant;
+//
+//@Measurement(name = "optical_data")
+//@Getter
+//@NoArgsConstructor
+//@ToString(of = {"time", "uuid", "optical_value"})
+//public class OpticalData {
+//
+//    @TimeColumn
+//    @Column(name = "time")
+//    private Instant time;
+//
+//    @Column(name = "uuid")
+//    private String uuid;
+//
+//    @Column(name = "optical_value")
+//    private int optical_value;
+//
+//
+//    public OpticalData(String uuid, int optical_value) {
+//        this.optical_value = optical_value;
+//        this.uuid = uuid;
+//
+//        this.time = Instant.now();
+//    }
+//
+//    public OpticalData(Instant time, String uuid, int optical_value) {
+//        this.time = time;
+//        this.optical_value = optical_value;
+//        this.uuid = uuid;
+//    }
+//
+//}
--- src/test/java/com/github/ckdgus08/service/DataServiceTest.java
+++ src/test/java/com/github/ckdgus08/service/DataServiceTest.java
@@ -1,101 +1,85 @@
 package com.github.ckdgus08.service;
 
 
-import com.github.ckdgus08.entity.OpticalData;
 import net.sf.yad2xx.Device;
 import net.sf.yad2xx.FTDIException;
 import net.sf.yad2xx.FTDIInterface;
-import net.sf.yad2xx.mpsse.Spi;
-import net.sf.yad2xx.mpsse.SpiMode;
-import org.influxdb.dto.BoundParameterQuery;
-import org.influxdb.dto.Point;
-import org.influxdb.dto.Query;
-import org.influxdb.dto.QueryResult;
-import org.influxdb.impl.InfluxDBResultMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.influxdb.InfluxDBTemplate;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.List;
 
-@SpringBootTest
 public class DataServiceTest {
-
-    @Autowired
-    private DataService dataService;
-    @Autowired
-    private InfluxDBTemplate<Point> influxDBTemplate;
 
     @Test
     void serial() throws IOException, FTDIException {
 
         int board_rate = 9600;
 
+        String libraryVersion = FTDIInterface.getLibraryVersion();
+        System.out.println("libraryVersion is " + libraryVersion);
+
         Device[] devices = FTDIInterface.getDevices();
 
-        if (devices.length == 0) {
-            System.out.println("No devices!!");
-            throw new IllegalStateException("연결된 기기가 없습니다.");
+        for (Device device : devices) {
+            System.out.println(device.getType());
         }
 
-        Device device = devices[0];
 
-        Spi spi = new Spi(device, board_rate, SpiMode.M0, false);
+//        if (devices.length == 0) {
+//            System.out.println("No devices!!");
+//            throw new IllegalStateException("연결된 기기가 없습니다.");
+//        }
+//
+//        Device device = devices[0];
+//
+//        Spi spi = new Spi(device, board_rate, SpiMode.M0, false);
+//
+//        byte[] bytes = spi.readBits(1000);
+//
+//        for (byte aByte : bytes) {
+//            System.out.println("aByte = " + aByte);
+//        }
 
-        byte[] bytes = spi.readBits(1000);
-
-        for (byte aByte : bytes) {
-            System.out.println("aByte = " + aByte);
-        }
-
-        spi.close();
 
     }
-
-    @Test
-    void write_data() throws IOException {
-        //given
-
-        File file = new File("/Users/cch/project/optical_fiber/src/main/resources/static/sample_data/sample2.txt");
-        List<String> list = Files.readAllLines(Paths.get(file.getAbsolutePath()), StandardCharsets.UTF_8);
-
-        // when
-        Instant instant = Instant.now();
-
-        for (int i = 0; i < 10000; i++) {
-            String value = list.get(i).split(",")[1].split("\\.")[0];
-            instant = instant.plusMillis(20);
-
-            OpticalData opticalData = new OpticalData(instant, "testUUID", Integer.parseInt(value));
-            //when
-            dataService.write(opticalData);
-        }
-        //then
-    }
-
-    @Test
-    public void select() {
-        Query query = BoundParameterQuery.QueryBuilder
-                .newQuery("SELECT * FROM optical_data")
-                .forDatabase("optical_fiber")
-                .create();
-
-        QueryResult queryResult = influxDBTemplate.query(query);
-        InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
-        // thread-safe - can be reused
-        List<OpticalData> testMeasurementList = resultMapper.toPOJO(queryResult, OpticalData.class);
-
-        for (OpticalData opticalData : testMeasurementList) {
-            System.out.println(opticalData.toString());
-        }
-    }
+//
+//    @Test
+//    void write_data() throws IOException {
+//        //given
+//
+//        File file = new File("/Users/cch/project/optical_fiber/src/main/resources/static/sample_data/sample2.txt");
+//        List<String> list = Files.readAllLines(Paths.get(file.getAbsolutePath()), StandardCharsets.UTF_8);
+//
+//        // when
+//        Instant instant = Instant.now();
+//
+//        for (int i = 0; i < 10000; i++) {
+//            String value = list.get(i).split(",")[1].split("\\.")[0];
+//            instant = instant.plusMillis(20);
+//
+//            OpticalData opticalData = new OpticalData(instant, "testUUID", Integer.parseInt(value));
+//            //when
+//            dataService.write(opticalData);
+//        }
+//        //then
+//    }
+//
+//    @Test
+//    public void select() {
+//        Query query = BoundParameterQuery.QueryBuilder
+//                .newQuery("SELECT * FROM optical_data")
+//                .forDatabase("optical_fiber")
+//                .create();
+//
+//        QueryResult queryResult = influxDBTemplate.query(query);
+//        InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
+//        // thread-safe - can be reused
+//        List<OpticalData> testMeasurementList = resultMapper.toPOJO(queryResult, OpticalData.class);
+//
+//        for (OpticalData opticalData : testMeasurementList) {
+//            System.out.println(opticalData.toString());
+//        }
+//    }
 
 
 }
